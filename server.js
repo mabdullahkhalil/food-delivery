bebdf0e6af9cdd80efb26f8f75b492eeac4bda13
Muhamamd Abdullah Khalil <muhammadabdullahkhalil@gmail.com>
2018-08-30 05:45:10 +0500

    facebook authentication of token

--- server.js
+++ server.js
@@ -3,15 +3,41 @@ var express = require("express");
 var app = express();
 var cors = require('cors');
 var bodyParser = require("body-parser");
+var passport = require("passport")
+var FacebookTokenStrategy = require('passport-facebook-token');
 
+
+
+app.use(passport.initialize());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+
+passport.use(new FacebookTokenStrategy({
+    clientID: "515902558881080",
+    clientSecret: "9cf7a42a9fb07e849c29f503f76a6c40"
+  }, function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({facebookId: profile.id}, function (error, user) {
+      return done(error, user);
+    });
+  }
+));
 
 var routes = require('./routes/authRoutes'); //importing route
 // routes(app);
 
 app.use('/api/',routes)
+
+app.post('/auth/facebook/token',
+  passport.authenticate('facebook-token'),
+  function (req, res) {
+  	console.log("trying to authenticate facebook")
+  	console.log(req.user)
+    // do something with req.user
+    res.send(req.user? 200 : 401);
+  }
+);
 
 
 // const PORT = 8081
