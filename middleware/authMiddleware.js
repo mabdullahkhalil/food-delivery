d04e93cc4315c59dea676a2f3d206f8785e0b52c
Muhamamd Abdullah Khalil <muhammadabdullahkhalil@gmail.com>
2018-10-08 23:48:28 +0500

    user role changing

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,7 +6,7 @@ exports.ensureCorrectUserRole= async(req,res,next) => {
 	try {
 		let userid = await jwtdecode(req.headers.token)
 		let user = await db.User.findOne({"_id": mongoose.Types.ObjectId(userid)})
-		if (user.userRole === "admin") {
+		if (user.userRole === "admin" || user.userRole === "customer") {
 			next();
 		} else {
 			res.status(401).json({message: 'You are not authorized for this task'})
